2da86307d1236dbc7ecc4df9abc76a40c2af25d6
huty1992 <34893733+huty1992@users.noreply.github.com>
2017-12-28 17:49:16 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+##estabilish a function of matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(matrix0 = matrix()) {
+matrixinverted<-NULL
+set<-function(y){
+matrix0<<-y
+matrixinverted<<-NULL
+}
+
+get<-function()matrix0
+SetInverse<-function(invert) matrixinverted<<-invert
+getInverse<- function()matrixinverted
+list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## invert the matrix if it was never inverted before as previously examined
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve<-function(matrix0,...){
+
+matrixinverted<-matrix0$getInverse()
+if(!is.null(matrixinverted)){
+
+message("getting cached inverted matrix")
+
+return(matrixinverted)
+}
+invertmatrix<-matrix0$get()
+matrixinverted<-solve(invertmatrix,...)
+matrix0$setInv(invert)
+matrixinverted
+
 }
